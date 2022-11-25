b2d736527d7d31b1a6ef1bd802a94da2a18cc4de
Blanca Ubis <ubis.blanca@gmail.com>
2022-11-25 11:30:54 +0100

    Change API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,12 @@
 const callToApi = () => {
-    return fetch(`https://adalab-api.herokuapp.com/api/random/word/`)
-      .then((response) => response.json())
-      .then((response) => {
-        // La respuesta del API es un objeto que dentro tiene la propiedad results que es un array
-        // Recorremos el array results obteniendo solo el nombre
-        // Esto lo hacemos para limpiar la respuesta de la API antes de devolverla al componente App
-        // Esto es opcional pero recomendado
-        const result = response.word
-        // Retornamos los resultados de la API al componente App
-        return result;
-      });
-  };
-  
-  export default callToApi;
+  return fetch(`https://dev.adalab.es/api/random/word`)
+    .then((response) => response.json())
+    .then((response) => {
+      // La respuesta del API es un objeto que dentro tiene la propiedad word que es un string
+      const result = response.word;
+      // Retornamos los resultados de la API al componente App
+      return result;
+    });
+};
+
+export default callToApi;
